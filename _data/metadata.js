9f31042a1e958a40cf7cc60dca794075eb146b40
Sean Lunsford <sean@lunsford.tech>
2023-09-09 17:31:37 +0300

    Add CSS/fonts and update header and footer

--- _data/metadata.js
+++ _data/metadata.js
@@ -5,6 +5,8 @@ module.exports = {
 	"author": {
 		"name": "Sean Lunsford",
 		"email": "sean@seanlunsford.com",
-		"url": "https://seanlunsford.com/"
+		"url": "https://seanlunsford.com/",
+		"mastodon_url": "https://data-folks.masto.host/@sean",
+		"threads_url": "https://www.threads.net/@splunsford"
 	}
 }
